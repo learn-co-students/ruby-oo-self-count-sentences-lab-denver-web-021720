8f9b8a547a1ddc234ea45695b7f596904ce01818
joegelay <joegelay@gmail.com>
2020-02-23 09:02:25 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,28 @@ require 'pry'
 
 class String
 
+  # helper method for below methods
+  def sentence_type?(punctuation)
+    if self[-1] == punctuation
+      return true 
+    else 
+      return false 
+    end 
+  end 
+
   def sentence?
-    
+    sentence_type?(".")
   end
 
   def question?
-
+    sentence_type?("?")
   end
 
   def exclamation?
-
+    sentence_type?("!")
   end
 
   def count_sentences
-
+    result = self.split(/[?!.] /).count
   end
 end
